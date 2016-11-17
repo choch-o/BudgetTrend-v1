c5525fdac92eb21854f6cdee896ffcf68d6c4c8f
hyunsungcho <hscho1226@gmail.com>
2016-11-18 04:29:17 +0900

    Initial project setup + Promises.js

---
+++ app/components/Promises.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Promises extends React.Component {
+  render() {
+    return (
+      <div className="promise">
+        <div className="panel">
+          <div className="panel-body">
+            <h1>Promises</h1>
+            <p></p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Promises;
